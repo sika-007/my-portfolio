82ea7a37ee4df356b33db767ad90e06a5ef3e51b
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-04-04 22:38:41 +0100

    Fallback for 3D model laid out and styled

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei"
 
 const Loader = () => {
+
+  const { progress } = useProgress()
+  console.log(progress)
+
   return (
-    <div>Loader</div>
+    <Html>
+      <span className="canvas-loader"></span>
+      <p className="text-[14px] text-[#f1f1f1] mt-10 font-extrabold">{progress.toFixed(2)}%</p>
+    </Html>
   )
 }
 
