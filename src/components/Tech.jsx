3983e6798ca4360260e9a54373ea333ba34548e1
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-04-10 14:56:56 +0100

    Technologies section laid out and styled

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../HOC"
+import { technologies } from "../constants"
 
 const Tech = () => {
+
+  const techModels =  technologies.map((technology, index) => (
+    <div className="w-28 h-28 flex flex-col items-center" key={index}>
+      <BallCanvas icon={technology.icon}/>
+      <p>{technology.name}</p>
+    </div>
+  ))
+
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {techModels}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "")
