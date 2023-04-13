08873f1e26e5d26a41e2dedfa1d0ce6466067807
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-04-13 21:52:59 +0100

    Earth model imported nad implemented

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader"
+import { Mesh } from "three";
 
 const Earth = () => {
+
+  const Earth = useGLTF("../../../../planet/scene.gltf")
+
   return (
-    <div>Earth</div>
+    <mesh>
+      <primitive 
+        object={Earth.scene}
+        scale={2.5}
+        rotation-y={0}
+      />s
+    </mesh>
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{ 
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6]
+       }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas
