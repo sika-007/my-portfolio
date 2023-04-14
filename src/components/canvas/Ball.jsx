ea7e404b3ef5b60c981df35f680edc00154ad12a
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-04-14 23:02:04 +0100

    frameloop removed for desktop ball models

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -23,7 +23,6 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop="demand"
       gl={{ preserveDrawingbuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
