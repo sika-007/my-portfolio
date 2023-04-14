9f76124cda1e5a8d5b673f5c1c62ec3c1dfc292e
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-04-14 23:04:19 +0100

    logic for rendering cards on mobile implemented

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
 import { BallCanvas } from "./canvas"
 import { SectionWrapper } from "../HOC"
 import { technologies } from "../constants"
+import BallCard from "./BallCard"
 
 const Tech = () => {
+
+  const [canCarryModels, setCanCarryModels] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1000px)");
+
+    setCanCarryModels(!mediaQuery.matches);
+
+    function handleMQChange(e){
+      setCanCarryModels(!e.matches)
+    }
+
+    mediaQuery.addEventListener('change', handleMQChange)
+
+    return () =>  mediaQuery.removeEventListener("change", handleMQChange)
+  }, [])
 
   const techModels =  technologies.map((technology, index) => (
     <div className="w-28 h-28 flex flex-col items-center" key={index}>
@@ -11,9 +29,16 @@ const Tech = () => {
     </div>
   ))
 
+  const techCards = technologies.map((technology, index) => (
+    <div className="w-28 h-28 flex flex-col items-center" key={index}>
+      <BallCard icon={technology.icon}/>
+      <p>{technology.name}</p>
+    </div>
+  ))
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {techModels}
+      {canCarryModels ? techModels : techCards }
     </div>
   )
 }
