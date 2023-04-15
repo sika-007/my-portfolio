bc52d05427c6112f2d31d6ed60abc77acebe3e66
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-04-15 09:19:17 +0100

    navigation fix for mobile devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute bottom-10 xs:bottom-10 w-full flex justify-center items-center">
+      <div className="absolute bottom-15 xs:bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div 
