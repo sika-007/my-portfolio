4013842f9215a87f3a233b812677748425c68019
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-08 08:16:43 +0100

    Technologies section updated

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -13,32 +13,45 @@ const Tech = () => {
 
     setCanCarryModels(!mediaQuery.matches);
 
-    function handleMQChange(e){
+    function handleMQChange(e) {
       setCanCarryModels(!e.matches)
     }
 
     mediaQuery.addEventListener('change', handleMQChange)
 
-    return () =>  mediaQuery.removeEventListener("change", handleMQChange)
+    return () => mediaQuery.removeEventListener("change", handleMQChange)
   }, [])
 
-  const techModels =  technologies.map((technology, index) => (
+  const techModels = technologies.map((technology, index) => (
     <div className="w-28 h-28 flex flex-col items-center" key={index}>
-      <BallCanvas icon={technology.icon}/>
+      <BallCanvas icon={technology.icon} />
       <p>{technology.name}</p>
     </div>
   ))
 
-  const techCards = technologies.map((technology, index) => (
-    <div className="w-28 h-28 flex flex-col items-center" key={index}>
-      <BallCard icon={technology.icon}/>
-      <p>{technology.name}</p>
-    </div>
-  ))
+  const techCards = technologies.map((technology, index) => {
+
+    if (index === 11 || index === 13) {
+      return (
+        <div className="w-28 h-28 flex flex-col items-center" key={index}>
+          <BallCard padding="py-2" smHeight="h-16" icon={technology.icon} />
+          <p>{technology.name}</p>
+        </div>
+      )
+    } else {
+      return (
+        <div className="w-28 h-28 flex flex-col items-center" key={index}>
+          <BallCard icon={technology.icon} />
+          <p>{technology.name}</p>
+        </div>
+      )
+    }
+
+  })
 
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {canCarryModels ? techModels : techCards }
+      {canCarryModels ? techModels : techCards}
     </div>
   )
 }
