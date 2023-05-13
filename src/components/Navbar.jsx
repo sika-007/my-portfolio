66bd5dc1a31749e421e4853040e2a58b0933bc3e
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-13 23:20:49 +0100

    Added My github link to navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import { styles } from '../style';
 import { navLinks } from '../constants';
-import { pnglogo } from '../assets';
+import { github, pnglogo } from '../assets';
 
 /* Font family used for logo is the megrim font */
 
@@ -29,14 +29,19 @@ const Navbar = () => {
         <Link className='flex items-center sm:gap-2 gap-5 sm:grow' to="/" onClick={() => {
           setActive("")
           scrollTo(0, 0)
-          }}>
-            <img src={pnglogo} alt="logo" loaing="lazy" className="h-6 object-contain" />
-            <p className='text-white text-left text-[16px] font-light cursor-pointer'>Nsikak <span className='sm:inline block'>Thomas</span></p>
+        }}>
+          <img src={pnglogo} alt="logo" loaing="lazy" className="h-6 object-contain" />
+          <p className='text-white text-left text-[16px] font-light cursor-pointer'>Nsikak <span className='sm:inline block'>Thomas</span></p>
         </Link>
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
+        <ul className='list-none hidden md:flex flex-row gap-10'>
+          <li>
+            <a href="https://github.com/sika-007">
+              <img src={github} alt="github" className='w-7 h-7' />
+            </a>
+          </li>
           {linkElements}
         </ul>
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+        <div className="md:hidden flex flex-1 justify-end items-center">
           <div className='flex flex-col gap-1 items-end' onClick={(() => setToggle(prev => !prev))}>
             <div className={`${toggle ? "w-6" : "w-5"} h-[3px] origin-center bg-white ${toggle && "rotate-45 translate-y-[7px]"} transition-all duration-300`} />
             <div className={`w-4 h-[3px] bg-white ${toggle && "opacity-0 rotate-45"} transition-all duration-300`} />
@@ -45,7 +50,13 @@ const Navbar = () => {
           <div className={`${toggle ? "translate-x-0 opacity-100" : "translate-x-32 opacity-0"} transition-all flex p-6 black-gradient absolute top-20 right-0 my-2 mx-4 min-w-[140px] z-10 rounded-sm`}>
             <ul className='list-none flex flex-col items-start gap-4'>
               {linkElements}
-            </ul> 
+              <li>
+                <a className='flex items-center flex-row-reverse gap-1' href="https://github.com/sika-007">
+                  <img src={github} alt="github" className='w-7 h-7' />
+                  <p className=' text-secondary font-poppins sm:hover:text-white sm:text-[18px] font-medium'>Github</p>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
